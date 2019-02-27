4c866e218fb02e5da0994226adf68bc02a6bce72
darling-trace-1821 <kmanrique93@gmail.com>
2019-02-27 01:22:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,11 @@
 def square_array(array)
   squared_array = []
-  squared_array << array.each {|x| x ** 2}
+  array.each {|x| squared_array << x ** 2}
   squared_array
 end
+
+#def square_array(array)
+  #arr = []
+  #array.each { |i| arr << i ** 2 }
+ # arr
+#end
