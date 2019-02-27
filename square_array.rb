c9c90d5543955b5dd27e7b5bb7316e1681fb99fc
darling-trace-1821 <kmanrique93@gmail.com>
2019-02-27 01:18:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,4 @@
 def square_array(array)
-  #squared_array = []
-  #array.each {|x| squared_array << (x ** 2)}
-  #squared_array << el
+  squared_array = []
+  squared_array << array.each {|x| x ** 2}
 end
-
-array1 = [1, 2]
-
-puts array1.each
